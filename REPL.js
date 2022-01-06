ce2752dc7bd96746fad324a5d0b6f2d58ce2aa67
Manish <manishgoyal6681@gmail.com>
2022-01-06 17:40:47 +0530

    complete explaination of REPL

---
+++ REPL.js
@@ -0,0 +1,25 @@
+// RemotePlayback
+// 1: js expressions
+// we can do the maths sum in the node
+
+
+// 2: use variable
+
+
+// 3: multiline codes
+// var x = 0;
+// do{
+//     x++;
+//     console.log(`my x valus is ${x}`);
+// }while(x<5);
+
+
+// 4: use (_) to get the last result
+ // we will get the previous value directly
+
+// 5: we can use editor mode
+// const name =(myname) => {console.log(`my name is $(myname);`);}
+// name("Manish Goyal")
+
+// then press ctrl + debugger;
+
